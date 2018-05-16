e2c3044a667db5f26b92b18efe873e2ceb699022
GreenAlmonah <39312646+unhelmbot@users.noreply.github.com>
2018-05-16 23:25:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        main
+# Purpose:
+#
+# Author:      unhelmbot
+#
+# Created:     16.05.2018
+# Copyright:   (c) unhelmbot
+# Licence:     <no licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+from qefunc_script import qefunc
